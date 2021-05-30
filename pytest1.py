43f15f666873176382091012b96a3107a67e1ead
nagaraj-nex <82175614+nagaraj-nex@users.noreply.github.com>
2021-05-30 08:07:34 +0530

    Add files via upload

---
+++ pytest1.py
@@ -0,0 +1,54 @@
+import os
+from pybfe.client.session import Session
+from intentionet.bfe.proto import api_gateway_pb2 as api
+
+def create_session():
+    os.environ['BFE_SSL_CERT'] = "../projects/pybfe-guide/nexbfe.crt"
+    BFE_HOST = "batfish.nexariacloud.com"
+    BFE_PORT = 443
+    bf = Session(host=BFE_HOST, port=BFE_PORT)
+    return bf
+
+def clear_all(bf):
+    '''Deletes all networks except the default'''
+    networks = bf.list_networks()
+    for network in networks:
+        if network.startswith("Example"):
+            #print(network)
+            bf.set_network(network)
+        else:
+            bf.delete_network(network)
+
+
+REF_SNAPSHOT = ''
+NEW_SNAPSHOT = ''
+NETWORK_NAME="bgp_test_lab"
+bf = create_session()
+bf.set_network(NETWORK_NAME)
+print(bf.list_networks())
+
+snapshots = bf.list_snapshots()
+#case when tere are just 2 snapshots, where reference snapshot is called baseline and is manually uploaded
+if len(snapshots) == 2:
+    for snapshot in snapshots:
+        if snapshot.startswith('baseline'):
+            REF_SNAPSHOT = snapshot
+        else: NEW_SNAPSHOT = snapshot
+
+def get_compare_metadata_results(bf: Session, snapshot_name: str, reference_snapshot_name: str):
+    """
+    Gets snapshot comparison results.
+    
+    Returns a map from comparison type to either a count of changes or whether that type has changed.
+    """
+    resp = bf._api_gw.GetSnapshotComparisonMetadata(
+        api.GetSnapshotComparisonMetadataRequest(
+            network_name=bf.network,
+            snapshot_name=snapshot_name,
+            reference_snapshot_name=reference_snapshot_name
+        )
+    )
+    return resp
+
+response = get_compare_metadata_results(bf, REF_SNAPSHOT, NEW_SNAPSHOT)
+print(response)
